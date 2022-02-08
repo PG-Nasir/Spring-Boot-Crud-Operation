6b78c820a665a1029f885da310a0324e0ca5da9a
Nasir <pgnasir17@gmail.com>
2022-02-08 19:32:52 +0600

    first commit

---
+++ spring-crud-operation/spring-crud-operation/src/main/java/pg/nasir/springcrudoperation/repository/BooksRepository.java
@@ -0,0 +1,10 @@
+package pg.nasir.springcrudoperation.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import pg.nasir.springcrudoperation.model.Books;
+
+public interface BooksRepository extends CrudRepository<Books, Integer>  
+{  
+
+}
---
+++ spring-crud-operation/spring-crud-operation/src/test/java/pg/nasir/springcrudoperation/SpringCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package pg.nasir.springcrudoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
